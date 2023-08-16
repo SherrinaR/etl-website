b07c4c68f9977366e63de9e6572282c21f343dba
Sherrina <robinsonsherrina@gmail.com>
2023-08-15 21:16:44 -0400

    added navbar links and menu icon to header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,55 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  // update the state for clicking the menu icon
+  const [click, setClick] = useState(false);
+
+  // Will reverse the state when clicked
+  const handleClick = () => setClick(!click);
+
+  const closeMobileMenu = () => setClick(false);
   return (
     <>
-        <nav className='navbar'>
-            <div className='navbar-container'>
+        <nav className="navbar">
+            <div className="navbar-container">
                 <Link to="/" className="navbar-logo">
-                    TRVL <i className='fab fa-typo3' />
+                    ETL Ministries <i class="uil uil-clinic-medical" />
                 </Link>
+                <div className="menu-icon" onClick={handleClick}>
+                  <i className={click ? "uil uil-times" : "uil uil-bars"} />
+                </div>
+                <ul className={click ? "nav-menu active" : "nav-menu"}>
+                  <li className="nav-item">
+                    <Link to="/" className="nav-links" onclick={closeMobileMenu}>
+                      HOME
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/about" className="nav-links" onclick={closeMobileMenu}>
+                      ABOUT US
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/biblestudy" className="nav-links" onclick={closeMobileMenu}>
+                      BIBLE STUDY
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/events" className="nav-links" onclick={closeMobileMenu}>
+                      EVENTS
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/mission" className="nav-links" onclick={closeMobileMenu}>
+                      MISSION
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/give" className="nav-links" onclick={closeMobileMenu}>
+                      GIVE
+                    </Link>
+                  </li>
+                </ul>
             </div>
         </nav>
     </>
