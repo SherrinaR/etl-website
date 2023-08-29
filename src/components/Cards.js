6dce22f98f654b002571005a0a7b3b4e1bc87f94
Sherrina <robinsonsherrina@gmail.com>
2023-08-28 21:33:48 -0400

    added more card items, updated format

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,12 +9,32 @@ function Cards() {
         <h1>The Lord awaits you</h1>
         <div className='cards--container'>
             <div className='cards--wrapper'>
-                <ul className='cards--items'>
+                <ul className='cards--item'>
                     <CardItem 
                         src='images\bible-study.jpg'
                         text='In the beginning God created the heavens and the earth'
-                        label='Ministry'
+                        label='Bible Ministry'
                         path='/services'
+                    />
+                    <CardItem 
+                        src='images\bible-study.jpg'
+                        text='In the beginning God created the heavens and the earth'
+                        label='Prayer Ministry'
+                        path='/contact'
+                    />
+                </ul>
+                <ul className='cards--item'>
+                    <CardItem 
+                        src='images\bible-study.jpg'
+                        text='In the beginning God created the heavens and the earth'
+                        label='Marriage Ministry'
+                        path='/contact'
+                    />
+                    <CardItem 
+                        src='images\bible-study.jpg'
+                        text='In the beginning God created the heavens and the earth'
+                        label='Mission Work'
+                        path='/mission'
                     />
                 </ul>
             </div>
